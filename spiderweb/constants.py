7ee119d42d54409419f5bfe6e271d5d6beff2944
Joe Kaufeld <opensource@joekaufeld.com>
2024-09-09 01:44:09 -0400

    :bookmark: release 1.1.0

--- spiderweb/constants.py
+++ spiderweb/constants.py
@@ -2,7 +2,7 @@ from peewee import DatabaseProxy
 
 DEFAULT_ALLOWED_METHODS = ["POST", "GET", "PUT", "PATCH", "DELETE"]
 DEFAULT_ENCODING = "UTF-8"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
 REGEX_COOKIE_NAME = r"^[a-zA-Z0-9\s\(\)<>@,;:\/\\\[\]\?=\{\}\"\t]*$"
