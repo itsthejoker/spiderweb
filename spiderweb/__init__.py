978d51b750ba349065ea860b031fc2292a501cbb
Joe Kaufeld <opensource@joekaufeld.com>
2026-04-11 00:06:28 -0400

    ✨ export ASGIHandler from package

--- spiderweb/__init__.py
+++ spiderweb/__init__.py
@@ -1,3 +1,4 @@
 from spiderweb.main import SpiderwebRouter  # noqa: F401
 from spiderweb.middleware import *  # noqa: F401, F403
 from spiderweb.constants import __version__ as __version__
+from spiderweb.asgi import ASGIHandler  # noqa: F401
